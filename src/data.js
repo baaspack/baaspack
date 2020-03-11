96c9638ab9118b3a8b8a60e12d0e5570fccfe9a3
Liela Rotschy <lielarotschy@gmail.com>
2020-03-11 15:03:24 -0400

    add routes for users, messages, add seed data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const usersSeed = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch'
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids'
+  }
+};
+export const messagesSeed = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1'
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2'
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import uuidv4 from 'uuid/v4';
+import { usersSeed, messagesSeed } from './data';
 
 const app = express();
 
-app.use(cors());
+let messages = messagesSeed;
+let users = usersSeed;
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
+});
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const { [req.params.messageId]: message, ...otherMessages } = messages;
+
+  messages = otherMessages;
+
+  return res.send(message);
 });
 
 app.listen(process.env.PORT, () => console.log('Example app listening'));
