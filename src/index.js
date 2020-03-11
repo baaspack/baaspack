99aaf9efb2edf4f215ae557f3560b872bdfe2d79
Holden Hinkle <holdenhinkle@Holdens-MacBook-Pro.local>
2020-03-11 18:05:13 -0400

    setup mongoDb and create user and message models. seed db with user and message data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
-import models from './models';
+import models, { connectDb } from './models';
 import routes from './routes';
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   req.context = {
     models,
-    me: models.users[1],
+    me: models.users[1]
   };
 
   next();
@@ -24,4 +24,51 @@ app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-app.listen(process.env.PORT, () => console.log('Example app listening'));
+const eraseDatabaseOnSync = false;
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({})
+    ]);
+
+    createUsersWithMessages();
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`)
+  );
+});
+
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'Rotschy'
+  });
+
+  const user2 = new models.User({
+    username: 'Moskovich'
+  });
+
+  const message1 = new models.Message({
+    text: 'Published the Road to lear React',
+    user: user1.id
+  });
+
+  const message2 = new models.Message({
+    text: 'Hello neighbor!',
+    user: user2.id
+  });
+
+  const message2 = new models.Message({
+    text: 'Top of the morning to you, sir!',
+    user: user2.id
+  });
+
+  await message1.save();
+  await message2.save();
+  await message3.save();
+
+  await user1.save();
+  await user2.save();
+};
