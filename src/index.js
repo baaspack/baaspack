c01c6568a531f778e5db6cf0de7d7f87bc568242
Liela Rotschy <lielarotschy@gmail.com>
2020-03-11 13:58:27 -0400

    add .env, set up cors, import express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import 'dotenv/config';
-import saySomething from './my-other-file.js';
-console.log('Hello Node.js project.');
-console.log(process.env.MY_SECRET);
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT, () => console.log('Example app listening'));
