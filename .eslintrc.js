726f652e096aeff3ca39216d297c1cf025fa29c4
Ido <imidom@gmail.com>
2020-04-08 16:26:49 -0400

    Allow unused vars with underscore prefix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   "extends": "airbnb-base",
   "rules": {
-    "no-console": "off"
+    "no-console": "off",
+    "no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ]
   }
 };
