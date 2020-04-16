b9214d780f54385cee597bd8f76691c584cbd7d0
Holden Hinkle <holdenhinkle@gmail.com>
2020-04-16 10:30:05 -0400

    update broadcast method to broadcast messages to a channel or to all active connections

--- src/websockets/routeHandlers.js
+++ src/websockets/routeHandlers.js
@@ -3,6 +3,7 @@ const createWebsocketRouteHandlers = (wss) => {
 
   const handlers = {
     handleHttpRequest(action, collection, data) {
+      console.log('MADE IT HERE - createWebsocketRouteHandlers');
       const dataCopy = unfreezeObject(data);
       this.broadcast(Object.assign({}, { action, collection, response: dataCopy }));
     },
@@ -10,8 +11,22 @@ const createWebsocketRouteHandlers = (wss) => {
       client.send(JSON.stringify(message));
     },
     broadcast(message) {
-      const channelType = message.response.channelType || null;
-      const channelId = message.response.channelId || null;
+      let channelType;
+      let channelId;
+
+      // messages for http responses will have a response object
+      // regular messages will not
+      if (message.response && message.response.channelType && message.response.channelId) {
+        channelType = message.response.channelType;
+        channelId = message.response.channelId;
+      } else if (message.channelType && message.channelId) {
+        channelType = message.channelType;
+        channelId = message.channelId;
+      } else {
+        channelType = null;
+        channelId = null;
+      }
+
       const channelName = `${channelType}_${channelId}`;
 
       if (channelType && channelId && wss.channels[channelName]) {
