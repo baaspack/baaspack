f6d9c8cbab7e0c49ca034fca6f1f8f6511d24243
Holden Hinkle <holdenhinkle@gmail.com>
2020-04-19 02:12:32 -0400

    pushing this file with lots of comments before I clean it up incase I need to reference it again

--- src/websockets/routeHandlers.js
+++ src/websockets/routeHandlers.js
@@ -1,26 +1,71 @@
 const createWebsocketRouteHandlers = (wss) => {
   const unfreezeObject = (obj) => JSON.parse(JSON.stringify(obj));
 
+  const isUsermeta = (collection) => {
+    return collection === 'usersmeta';
+  }
+
   const handlers = {
     handleHttpRequest(action, collection, data) {
-      console.log('MADE IT HERE - createWebsocketRouteHandlers');
-      const dataCopy = unfreezeObject(data);
-      this.broadcast(Object.assign({}, { action, collection, response: dataCopy }));
+      // const dataCopy = unfreezeObject(data);
+      // const message = Object.assign({}, { action, collection, response: dataCopy });
+
+      // console.log('got here - handleHttpRequest');
+      // console.log('action', action);
+      // console.log('collection', collection);
+      // console.log('data', data);
+
+      // if (isUsermeta(collection)) {
+      //   console.log('got here - isUsermeta(collection)');
+      //   this.sendToOwner(message);
+      // } else {
+      //   this.broadcast(message);
+      // }
     },
-    sendMessage(client, message) {
-      client.send(JSON.stringify(message));
+    sendToOwner(message) {
+      console.log('got here - sendToOwner(message)');
+      console.log('message', message);
+      console.log('message.response.userId', message.response.userId);
+      console.log(' wss', wss);
+      console.log(' wss.clients', wss.clients);
+
+      // if (wss.clients.length > 0) {
+      //   wss.clients.forEach((client) => {
+      //     console.log('got here - client.userId', client.userId);
+      //     if (client.userId === message.response.userId) {
+      //       console.log('got here - client.userId === message.response.userId');
+      //       this.sendMessage(client, message);
+      //     }
+      //   })
+      // }
+      wss.clients.forEach((client) => {
+        console.log('got here - client.userId', client.userId);
+        if (client.userId === message.response.userId) {
+          console.log('got here - client.userId === message.response.userId');
+          this.sendMessage(client, message);
+        }
+      })
+    },
+    sendMessageToUser(userId, message) {
+      wss.clients.forEach((client) => {
+        if (client.userId === userId) {
+          this.sendMessage(client, message);
+        }
+      })
+    },
+    broadcastButNotToOwner(message) {
+      console.log('got here - broadcastButNotToOwner');
+
+
+
+
+
     },
     broadcast(message) {
-      // if collection is usersmeta, only send to ws with userId set to usersId
-      // => we need to set a userId property to the correct wss.clients ws object
+      console.log('got here - broadcast');
 
-      if (message.collection === 'usersmeta') {
-        wss.clients.forEach(client => {
-          if (client.userId === message.response.userId) {
-            client.sendMessage(message)
-          }
-        });
-
+      if (isUsermeta(message.collection)) {
+        this.sendToOwner(message);
         return;
       }
 
@@ -28,9 +73,6 @@ const createWebsocketRouteHandlers = (wss) => {
 
       let channelType;
       let channelId;
-
-
-
 
       // messages for http responses will have a response object
       // regular messages will not
@@ -55,6 +97,9 @@ const createWebsocketRouteHandlers = (wss) => {
       } else {
         wss.clients.forEach((client) => this.sendMessage(client, message));
       }
+    },
+    sendMessage(client, message) {
+      client.send(JSON.stringify(message));
     },
   };
 
