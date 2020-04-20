33ab624347079277171cd7fdc5664f76da6e9a97
Holden Hinkle <holdenhinkle@gmail.com>
2020-04-20 15:00:29 -0400

    refactor broadcast method

--- src/websockets/routeHandlers.js
+++ src/websockets/routeHandlers.js
@@ -1,3 +1,5 @@
+// change response var name to resource
+
 const createWebsocketRouteHandlers = (wss) => {
   const unfreezeObject = (obj) => JSON.parse(JSON.stringify(obj));
 
@@ -5,16 +7,30 @@ const createWebsocketRouteHandlers = (wss) => {
     return collection === 'usersmeta';
   }
 
+  const getChannelInfo = (message) => {
+    let channelType;
+    let channelId;
+
+    if (message.response && message.response.channelType && message.response.channelId) {
+      channelType = message.response.channelType;
+      channelId = message.response.channelId;
+    } else if (message.channelType && message.channelId) {
+      channelType = message.channelType;
+      channelId = message.channelId;
+    } else {
+      channelType = null;
+      channelId = null;
+    }
+
+    return { channelType, channelId };
+  }
+
   const handlers = {
     handleHttpRequest(action, collection, data) {
       const dataCopy = unfreezeObject(data);
-      const message = Object.assign({}, { action, collection, response: dataCopy });
+      const message = Object.assign({}, { action, collection, httpRequest: true, response: dataCopy });
 
-      if (isUsermeta(collection)) {
-        this.sendToOwner(message);
-      } else {
-        this.broadcast(message);
-      }
+      this.broadcast(message);
     },
     sendToOwner(message) {
       wss.clients.forEach((client) => {
@@ -36,20 +52,7 @@ const createWebsocketRouteHandlers = (wss) => {
         return;
       }
 
-      let channelType;
-      let channelId;
-
-      if (message.response && message.response.channelType && message.response.channelId) {
-        channelType = message.response.channelType;
-        channelId = message.response.channelId;
-      } else if (message.channelType && message.channelId) {
-        channelType = message.channelType;
-        channelId = message.channelId;
-      } else {
-        channelType = null;
-        channelId = null;
-      }
-
+      const { channelType, channelId } = getChannelInfo(message);
       const channelName = `${channelType}_${channelId}`;
 
       if (channelType && channelId && wss.channels[channelName]) {
